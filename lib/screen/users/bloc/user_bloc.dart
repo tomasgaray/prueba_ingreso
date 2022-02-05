b65f18d2ebad12e7314cc11bb1307b740b9920a1
Tomas Garay <toasgaray07@gmail.com>
2022-02-04 23:19:53 -0600

    Solución a bug en asignación de usuarios locales/remotos

--- lib/screen/users/bloc/user_bloc.dart
+++ lib/screen/users/bloc/user_bloc.dart
@@ -9,50 +9,46 @@ class UserBloc extends Bloc<UserEvent, UserState> {
   final UserRepository userRepository;
 
   UserBloc(this.userRepository) : super(UserStarting()) {
-    on<AppStarted>((event, emit) async{
+    on<AppStarted>((event, emit) async {
       try {
         emit(UserLoading(message: "Obteniendo Usuarios..."));
         List<User> users = await userRepository.getUsersLocal();
-        users.isEmpty ?  await userRepository.getUsers(): users;
+        users = users.isEmpty ? await userRepository.getUsers() : users;
         emit(FindUsers(users: users));
-      }
-      catch (error) {
+      } catch (error) {
         emit(UserFailure(error: error.toString()));
       }
     });
 
-    on<SearchUserByName>((event, emit) async{
+    on<SearchUserByName>((event, emit) async {
       try {
         emit(UserLoading(message: "Buscando usuario..."));
-        List<User> users = await userRepository.searchUserByName(name:event.name);
-        users.isEmpty ? emit(ListEmpty()): emit(FindUsers(users: users));
-      }
-      catch (error) {
+        List<User> users =
+            await userRepository.searchUserByName(name: event.name);
+        users.isEmpty ? emit(ListEmpty()) : emit(FindUsers(users: users));
+      } catch (error) {
         emit(UserFailure(error: error.toString()));
       }
     });
 
-    on<GetLocalUsers>((event, emit) async{
+    on<GetLocalUsers>((event, emit) async {
       try {
         emit(UserLoading(message: "Obteniendo Usuarios..."));
         List<User> users = await userRepository.getUsersLocal();
-        users.isEmpty ?  await userRepository.getUsers(): users;
+        users.isEmpty ? await userRepository.getUsers() : users;
         emit(FindUsers(users: users));
-      }
-      catch (error) {
+      } catch (error) {
         emit(UserFailure(error: error.toString()));
       }
     });
 
-    on<UpdateLocalUsers>((event, emit) async{
+    on<UpdateLocalUsers>((event, emit) async {
       try {
         //Evitamos mostrar mensaje de cargando, porque esto solo esta actualizando los usuarios locales en segundo plano
         await userRepository.getUsers();
-      }
-      catch (error) {
+      } catch (error) {
         debugPrint(error.toString());
       }
     });
-
   }
 }
